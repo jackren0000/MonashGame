99314ae066039e0c3170b148963c425a674252bb
jackren0000 <91411340+jackren0000@users.noreply.github.com>
2023-06-23 23:40:31 +1000

    Update mb_websever.py

--- mb_websever.py
+++ mb_websever.py
@@ -34,6 +34,31 @@ class CNN(nn.Module):
     self.fc2 = nn.Linear(120, 84)
     self.fc3 = nn.Linear(84, 3)
 
+  def forward(self, x):
+    # Pass the first convolutional layer
+    x = self.pool_1(F.relu(self.conv1(x)))
+
+    # Pass the second convolutional layer
+    x = self.pool_2(F.relu(self.conv2(x)))
+
+    # Flatten x into one dimension
+    x = torch.flatten(x, 1)
+
+    # Pass the fully connected layer
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    x = self.fc3(x)
+
+    return x
+
+  def predict(self, x):
+    predictions = self(x)
+
+    # Get the predicted classes
+    _, predicted_classes = torch.max(predictions, dim=1)
+
+    return predicted_classes, x
+      
 # Instantiate your model and load its weights
 model = CNN()
 model.load_state_dict(torch.load('CNN_model.pth'))
